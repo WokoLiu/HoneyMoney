c40eb19b7c1fe7822faaa5bc9f964fb71188c6fb
Woko <banbooliu@gmail.com>
2022-09-18 00:15:37 +0800

    add timer to run

--- HoneyMoney/AppDelegate.swift
+++ HoneyMoney/AppDelegate.swift
@@ -9,12 +9,32 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     
-
+    let statusItem = NSStatusBar.system.statusItem(withLength: 100)
+    
+    func salaryWithSecond(salary: Int) -> Double {
+        return Double(salary) / 21.75 / 8 / 3600 / 1000
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        let secondMoney = salaryWithSecond(salary: 50000)
+        
+        if let button = statusItem.button {
+            button.title = "HoneyMoney"
+            button.action = #selector(actionHandler)
+            
+            var current = 0.0
+            Timer.scheduledTimer(withTimeInterval: 0.001, repeats: true) { timer in
+                current += secondMoney
+                button.title = String(current)
+            }
+        }
+    }
+
+    @objc func actionHandler(button: NSButton?) {
+        print("good start")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -24,7 +44,4 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-
-
 }
-
