9ce87eab8f8133ae700d93392f9e11b34c18d1bc
Woko <banbooliu@gmail.com>
2022-09-17 22:21:07 +0800

    init

---
+++ HoneyMoney/HoneyMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneyMoneyApp.swift
+//  HoneyMoney
+//
+//  Created by Woko on 2022/9/17.
+//
+
+import SwiftUI
+
+@main
+struct HoneyMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
