372627f9bcefe07a13190e33b1966202bb39f4cf
Woko <banbooliu@gmail.com>
2022-09-17 23:58:57 +0800

    init

---
+++ HoneyMoney/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HoneyMoney
+//
+//  Created by Woko on 2022/9/17.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
